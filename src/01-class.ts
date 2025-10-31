83d50ad6aac304aca8e8cb25679bd1690e46fe2a
Anyelo111 <anyelo1112020@gmail.com>
2025-10-31 17:45:30 -0400

    initial commit

---
+++ src/01-class.ts
@@ -0,0 +1,12 @@
+const date = new Date();
+date.getHours();
+date.getTime();
+date.toISOString();
+
+const date2 = new Date(1993, 1, 12); // 0 es Enero y 11 es Diciembre
+date2.getHours();
+date2.getTime();
+date2.toISOString();
+
+console.log(date)
+console.log(date2)
